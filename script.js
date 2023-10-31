04ed27c5e44b1d0e3d62836bb586a304c38aafc8
azhar <azhar@gmail.com>
2023-10-31 12:33:17 +0530

    new feature

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
     const taskInput = document.getElementById("task");
+    const prioritySelect = document.getElementById("priority");
     const addTaskButton = document.getElementById("addTask");
     const taskList = document.getElementById("taskList");
 
     addTaskButton.addEventListener("click", function () {
         const taskText = taskInput.value.trim();
+        const taskPriority = prioritySelect.value;
+
         if (taskText !== "") {
             const listItem = document.createElement("li");
+            listItem.classList.add(taskPriority); // Add priority class
             listItem.innerHTML = `
                 <span>${taskText}</span>
                 <button>Delete</button>
             `;
+
             taskList.appendChild(listItem);
             taskInput.value = "";
 
